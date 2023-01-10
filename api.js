27df7f4d6576053495472cf7728924ae49d9ca78
One <maheryrakotondrazaka@gmail.com>
2023-01-10 16:24:15 +0300

    initi

--- api.js
+++ api.js
@@ -1,6 +1,20 @@
 async function api_search(query, numResults, timePeriod, region) {
 
-    var url = `https://ddg-webapp-aagd.vercel.app/search?max_results=${numResults}&q=${query}`;
+    var url = `https://ddg-webapp-aagd.vercel.app/search?max_results=3&q=${query}`;
+
+    // const options = {
+    //     method: 'POST',
+    //     url: 'https://tldrthis.p.rapidapi.com/v1/model/abstractive/summarize-url/',
+    //     headers: {
+    //       'content-type': 'application/json',
+    //       'X-RapidAPI-Key': '9496288973msh4d676f7dbc9b866p1ebb33jsn55e2684623c2',
+    //       'X-RapidAPI-Host': 'tldrthis.p.rapidapi.com'
+    //     },
+    //     data: '{"url":"https://techcrunch.com/2019/08/12/verizon-is-selling-tumblr-to-wordpress-parent-automattic/","min_length":100,"max_length":300,"is_detailed":false}'
+    //   };
+      
+    //var url = `https://ddg-webapp-aagd.vercel.app/search?max_results=${numResults}&q=${query}`;
+
     if (timePeriod !== "") {
         url += `&time=${timePeriod}`;
     }
@@ -9,5 +23,6 @@ async function api_search(query, numResults, timePeriod, region) {
     }
 
     const response = await fetch(url);
+
     return await response.json();
 }
--- script.js
+++ script.js
@@ -13,7 +13,7 @@ chrome.storage.sync.get(["num_web_results", "web_access", "region"], (data) => {
 
 
 function showErrorMessage(e) {
-    console.info("WebChatGPT error --> API error: ", e);
+    console.info("MeloGPT error --> API error: ", e);
     var errorDiv = document.createElement("div");
     errorDiv.classList.add("web-chatgpt-error", "absolute", "bottom-0", "right-1", "dark:text-white", "bg-red-500", "p-4", "rounded-lg", "mb-4", "mr-4", "text-sm");
     errorDiv.innerHTML = "<b>An error occurred</b><br>" + e + "<br><br>Check the console for more details.";
@@ -23,13 +23,12 @@ function showErrorMessage(e) {
 
 function pasteWebResultsToTextArea(results, query) {
     let counter = 1;
-    let formattedResults = "Web search results:\n\n";
-    formattedResults = formattedResults + results.reduce((acc, result) => acc += `[${counter++}] "${result.body}"\nSource: ${result.href}\n\n`, "");
-
-    formattedResults = formattedResults + `\nCurrent date: ${new Date().toLocaleDateString()}`;
-    formattedResults = formattedResults + `\nInstructions: Using the provided web search results, write a comprehensive reply to the given prompt. Make sure to cite results using [[number](URL)] notation after the reference. If the provided search results refer to multiple subjects with the same name, write separate answers for each subject.\nPrompt: ${query}`;
-
-    textarea.value = formattedResults;
+    let length = 15894
+    results = results.replace(/[^a-zA-Z\d]/g, '');
+    let reduceddata =  results.length > length ? results.substring(0, length) : results;
+    reduceddata = "Summarize this and tell me " + query + " :" + reduceddata
+    //let formattedResults = 
+    textarea.value = reduceddata;
 }
 
 function pressEnter() {
@@ -64,10 +63,32 @@ function onSubmit(event) {
             }
 
             api_search(query, numWebResults, timePeriod, region)
-              .then(results => {
-                pasteWebResultsToTextArea(results, query);
+              .then( results => {
+
+                let options = {
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'application/json',
+                        'X-RapidAPI-Key': '9496288973msh4d676f7dbc9b866p1ebb33jsn55e2684623c2',
+                        'X-RapidAPI-Host': 'tldrthis.p.rapidapi.com'
+                    },
+                    body: `{"url": "${results[0].href}","min_length":100,"max_length":300,"is_detailed":false}`
+                };
+                
+               fetch('https://tldrthis.p.rapidapi.com/v1/model/abstractive/summarize-url/', options).then(response => response.json())
+               .then(response => {
+                pasteWebResultsToTextArea(response.article_text, query);
                 pressEnter();
                 isProcessing = false;
+               })
+               .catch(err => console.error(err));
+                // console.log("response.json() "  + response)
+               
+                    // .then(async response => {return await response.json()})
+                    // .then(response => console.log(response))
+                    // .catch(err => console.error(err));
+
+
               });
         } catch (error) {
             isProcessing = false;
@@ -82,11 +103,11 @@ function updateTitleAndDescription() {
         return;
     }
 
-    h1_title.textContent = "WebChatGPT";
+    h1_title.textContent = "MeloGPT (Melody is UwU :3)";
 
     const div = document.createElement("div");
     div.classList.add("w-full", "bg-gray-50", "dark:bg-white/5", "p-6", "rounded-md", "mb-10", "border");
-    div.textContent = "With WebChatGPT you can augment your prompts with relevant web search results for better and up-to-date answers.";
+    div.textContent = "Welcome to MeloGPT , The ChatGPT that can access the web , follow me https://twitter.com/Mahery2K";
     h1_title.parentNode.insertBefore(div, h1_title.nextSibling);
 
 }
@@ -117,7 +138,7 @@ function updateUI() {
 
     // Web access switch
     var toggleWebAccessDiv = document.createElement("div");
-    toggleWebAccessDiv.innerHTML = '<label class="web-chatgpt-toggle"><input class="web-chatgpt-toggle-checkbox" type="checkbox"><div class="web-chatgpt-toggle-switch"></div><span class="web-chatgpt-toggle-label">Search on the web</span></label>';
+    toggleWebAccessDiv.innerHTML = '<label class="web-chatgpt-toggle"><input class="web-chatgpt-toggle-checkbox" type="checkbox"><div class="web-chatgpt-toggle-switch"></div><span class="web-chatgpt-toggle-label">Search on the web (Melody :3)</span></label>';
     toggleWebAccessDiv.classList.add("web-chatgpt-toggle-web-access");
     chrome.storage.sync.get("web_access", (data) => {
         toggleWebAccessDiv.querySelector(".web-chatgpt-toggle-checkbox").checked = data.web_access;
@@ -131,24 +152,24 @@ function updateUI() {
 
 
     // Number of web results
-    var numResultsDropdown = document.createElement("select");
-    numResultsDropdown.classList.add("text-sm", "dark:text-white", "ml-0", "dark:bg-gray-800", "border-0");
+    // var numResultsDropdown = document.createElement("select");
+    // numResultsDropdown.classList.add("text-sm", "dark:text-white", "ml-0", "dark:bg-gray-800", "border-0");
 
-    for (let i = 1; i <= 10; i++) {
-        let optionElement = document.createElement("option");
-        optionElement.value = i;
-        optionElement.text = i + " result" + (i == 1 ? "" : "s");
-        numResultsDropdown.appendChild(optionElement);
-    }
+    // for (let i = 1; i <= 10; i++) {
+    //     let optionElement = document.createElement("option");
+    //     optionElement.value = i;
+    //     optionElement.text = i + " result" + (i == 1 ? "" : "s");
+    //     numResultsDropdown.appendChild(optionElement);
+    // }
 
-    chrome.storage.sync.get("num_web_results", (data) => {
-        numResultsDropdown.value = data.num_web_results;
-    });
+    // chrome.storage.sync.get("num_web_results", (data) => {
+    //     numResultsDropdown.value = data.num_web_results;
+    // });
 
-    numResultsDropdown.onchange = function () {
-        numWebResults = this.value;
-        chrome.storage.sync.set({ "num_web_results": this.value });
-    };
+    // numResultsDropdown.onchange = function () {
+    //     numWebResults = this.value;
+    //     chrome.storage.sync.set({ "num_web_results": this.value });
+    // };
 
     // Time period
     var timePeriodLabel = document.createElement("label");
@@ -205,7 +226,7 @@ function updateUI() {
     };
 
     toolbarDiv.appendChild(toggleWebAccessDiv);
-    toolbarDiv.appendChild(numResultsDropdown);
+    // toolbarDiv.appendChild(numResultsDropdown);
     toolbarDiv.appendChild(timePeriodDropdown);
     toolbarDiv.appendChild(regionDropdown);
 
@@ -220,7 +241,7 @@ function updateUI() {
     var footerDiv = document.createElement("div");
 
     var extension_version = chrome.runtime.getManifest().version;
-    footerDiv.innerHTML = "<a href='https://github.com/qunash/chatgpt-advanced' target='_blank' class='underline'>WebChatGPT extension v." + extension_version + "</a>. If you like the extension, please consider <a href='https://www.buymeacoffee.com/anzorq' target='_blank' class='underline'>supporting me</a>.";
+    footerDiv.innerHTML = "<a href='https://github.com/qunash/chatgpt-advanced' target='_blank' class='underline'>MeloGPT extension v." + extension_version + "</a>. If you like the extension, please consider <a href='https://twitter.com/Mahery2K' target='_blank' class='underline'>follow me / support me :3</a>.";
 
     var lastElement = bottomDiv.lastElementChild;
     lastElement.appendChild(footerDiv);
@@ -238,7 +259,7 @@ window.onload = () => {
             updateTitleAndDescription();
             updateUI();
         } catch (e) {
-            console.info("WebChatGPT error --> Could not update UI:\n", e.stack);
+            console.info("MeloGPT error --> Could not update UI:\n", e.stack);
         }
     }).observe(rootEl, { childList: true });
 };
